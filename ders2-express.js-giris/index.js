de2680efd59aba7547e0d6a17615b26af5ce2134
OFD16 <96663769+OFD16@users.noreply.github.com>
2023-01-24 22:48:01 +0300

    lesson 3

--- ders2-express.js-giris/index.js
+++ ders2-express.js-giris/index.js
@@ -1,16 +1,42 @@
-const exporess = require('express');
+const express = require('express');
 const data = require('./data.js');
-const server = exporess();
+const server = express();
+
+server.use(express.json()); //gelen bütün istekleri json formatında dönderir gibi bir şey
 
 server.get('/', (req, res) => {
-    res.send('Expressden Merhasdasdaba!');
+    res.send('Expressden Merhabalar!');
 });
 
 server.get('/actors', (req, res) => {
     res.status(200).json(data);
 });
 
+// server.get('/actors/:id', (req, res) => {
+//     const {id} = req.params;
+//     const actor = data.find( (actor) => actor.id === parseInt(id));
+
+//     if(actor){
+//         res.status(200).json(actor);
+//     }else{
+//         res.status(404).send("There is no result with you want to reach:...");
+//     }
+// });
+
+// server.get('/actors/:id', (req, res) => {
+//     console.log("req.query", req.query);
+//     const {id} = req.params;
+//     const actor = data.find( (actor) => actor.id === parseInt(id));
+
+//     if(actor){
+//         res.status(200).json(actor);
+//     }else{
+//         res.status(404).send("There is no result with you want to reach:...");
+//     }
+// });
+
 server.get('/actors/:id', (req, res) => {
+    console.log("req.body", req.body);
     const {id} = req.params;
     const actor = data.find( (actor) => actor.id === parseInt(id));
 
